9e96f9772f4e16d10994042f2307d7000a81f377
Rémi Patrizio <artemis.hacktivation@gmail.com>
2018-06-18 21:41:39 +0200

    Added main controller, main page & AJAX request to create a new Point

---
+++ src/Controller/AppController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Point;
+use App\Form\PointType;
+
+class AppController extends Controller
+{
+    /**
+     * Page d'accueil affichant la liste des points d'intérêts et une carte
+     * 
+     * @Route("/", name="homepage")
+     * @Template
+     */
+    public function index()
+    {
+    	$pointsOfInterest = $this->getDoctrine()->getManager()->getRepository('App:Point')->findAll();
+    	$newPointForm = $this->createNewPointForm(new Point(null, null, '42', '4'));
+
+        return [
+        	'pointsOfInterest' => $pointsOfInterest,
+        	'newPointForm' => $newPointForm->createView()
+        ];
+    }
+
+
+    /**
+     * @Route("/ajax/new-point", name="new_point")
+     * @Method("POST")
+     */
+    public function newPoint(Request $request)
+    {
+    	if (!$request->isXmlHttpRequest()) {
+	        return new JsonResponse(array('message' => 'You can access this only using Ajax!'), 400);
+	    }
+		$point = new Point();
+    	$em = $this->getDoctrine()->getManager();
+    	$newPointForm = $this->createNewPointForm($point);
+
+		$newPointForm->handleRequest($request);
+		if ($newPointForm->isSubmitted() && $newPointForm->isValid()) {
+			$em->persist($point);
+			$em->flush();
+
+        	return new JsonResponse(['2']);
+		}
+
+        return new JsonResponse(['1']);
+    }
+
+
+    private function createNewPointForm(Point $point)
+    {
+    	return $this->createForm(PointType::class, $point, [
+    		'method' => 'POST',
+    		'action' => $this->generateUrl('new_point')
+    	]);
+    }
+}
---
+++ src/Form/PointType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Point;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type;
+
+class PointType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', Type\TextType::class, [
+                'label' => "Nom du point d'intérêt"
+            ])
+            ->add('address', Type\TextType::class, [
+                'label' => "Adresse"
+            ])
+            ->add('latitude', Type\HiddenType::class)
+            ->add('longitude', Type\HiddenType::class)
+            ->add('submit', Type\SubmitType::class, [
+                'label' => "Valider"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Point::class,
+        ]);
+    }
+}
